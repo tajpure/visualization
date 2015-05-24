d1e955d20b8efd1fea8620c83b7428ced3e1989f
tajpure <tajpure@gmail.com>
2015-05-24 19:42:05 +0800

    add visual

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,28 @@ import play.api.mvc._
 object Application extends Controller {
   
   def main = Action {
-    Ok(views.html.main("visualization")(views.html.content("visualization")))
+    Ok(views.html.main("visualization"))
   }
   
+  def lex(source: String) = Action {
+//    val result = LexParser.lex(source)
+//    printf(result)
+    Ok(source)
+  }
+  
+  def parse(source: String) = Action {
+    Ok(source)
+  }
+  
+  def ir(source: String) = Action {
+    Ok(source)
+  }
+  
+  def target(source: String) = Action {
+    Ok(source)
+  }
+  
+  def exec(source: String) = Action {
+    Ok(source)
+  }
 }
---
+++ public/javascripts/visual.js
@@ -0,0 +1,57 @@
+var eidtor = null;
+
+function initEditor() {
+	editor = ace.edit("editor");
+	editor.setTheme("ace/theme/twilight");
+	var JavaScriptMode = require("ace/mode/sql").Mode;
+	editor.getSession().setMode(new JavaScriptMode());
+}
+
+var editor0 = null;
+
+function initEditor0() {
+	editor0 = ace.edit("editor0");
+	editor0.setTheme("ace/theme/twilight");
+	var JavaScriptMode = require("ace/mode/scheme").Mode;
+	editor0.getSession().setMode(new JavaScriptMode());
+}
+
+function doGet(_url) {
+	var request = $.ajax({
+		  url: _url,
+		  method: "GET"
+		});
+		 
+	request.done(function(msg) {
+		editor0.setValue(msg);
+	});
+	
+	request.fail(function( jqXHR, textStatus ) {
+		editor0.setValue("error!");
+	});
+}
+
+function lex() {
+	var source = editor.getSession().getValue();
+	doGet("lex?source="+source);
+}
+
+function parse() {
+	var source = editor.getSession().getValue();
+	doGet("parse?source="+source);
+}
+
+function ir() {
+	var source = editor.getSession().getValue();
+	doGet("ir?source="+source);
+}
+
+function target() {
+	var source = editor.getSession().getValue();
+	doGet("target?source="+source);
+}
+
+function exec() {
+	var source = editor.getSession().getValue();
+	doGet("exec?source="+source);
+}
